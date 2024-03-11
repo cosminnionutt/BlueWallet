ecfdcebdf459fa6e8f9d774ee88f41e0e27dec12
anonimus <148051645+cosminnionutt@users.noreply.github.com>
2024-03-11 15:36:41 +0200

    Add files via upload

---
+++ tests/App.test.js
@@ -0,0 +1,47 @@
+import assert from 'assert';
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import Settings from '../../screen/settings/Settings';
+import Selftest from '../../screen/selftest';
+import { BlueHeaderDefaultSub } from '../../BlueComponents';
+
+jest.mock('../../blue_modules/BlueElectrum', () => {
+  return {
+    connectMain: jest.fn(),
+  };
+});
+
+it('BlueHeaderDefaultSub works', () => {
+  const rendered = TestRenderer.create(<BlueHeaderDefaultSub />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+// eslint-disable-next-line jest/no-disabled-tests
+it.skip('Settings work', () => {
+  const rendered = TestRenderer.create(<Settings />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('Selftest work', () => {
+  const component = TestRenderer.create(<Selftest />);
+  const root = component.root;
+  const rendered = component.toJSON();
+  expect(rendered).toBeTruthy();
+  // console.log((root.findAllByType('Text')[0].props));
+
+  let okFound = false;
+  const allTests = [];
+  for (const v of root.findAllByType('Text')) {
+    let text = v.props.children;
+    if (text.join) {
+      text = text.join('');
+    }
+    if (text === 'OK') {
+      okFound = true;
+    }
+    allTests.push(text);
+    // console.log(text);
+  }
+
+  assert.ok(okFound, 'OK not found. Got: ' + allTests.join('; '));
+});
---
+++ tests/detox-build-release-apk.sh
@@ -0,0 +1,18 @@
+# script thats used to build & sign release APK in preparation for Detox e2e testing.
+
+# deleting old artifacts
+find android | grep '\.apk' --color=never | xargs -l rm
+
+# creating fresh keystore
+rm detox.keystore
+keytool -genkeypair -v -keystore detox.keystore -alias detox  -keyalg RSA -keysize 2048 -validity 10000 -storepass 123456 -keypass 123456 -dname  'cn=Unknown, ou=Unknown, o=Unknown, c=Unknown'
+
+# building release APK
+cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release && cd ..
+
+# signing
+echo wheres waldo?
+find $ANDROID_HOME | grep apksigner | grep -v jar
+mv ./android/app/build/outputs/apk/release/app-release-unsigned.apk ./android/app/build/outputs/apk/release/app-release.apk
+$ANDROID_HOME/build-tools/34.0.0/apksigner sign --ks detox.keystore   --ks-pass=pass:123456 ./android/app/build/outputs/apk/release/app-release.apk
+$ANDROID_HOME/build-tools/34.0.0/apksigner sign --ks detox.keystore   --ks-pass=pass:123456 ./android/app/build/outputs/apk/androidTest/release/app-release-androidTest.apk
